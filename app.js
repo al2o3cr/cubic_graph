aa5b1055190a19e4eed88eea38b1377e76eae690
Matt Jones <al2o3cr@gmail.com>
2012-05-06 22:08:58 -0400

    add expansion method and fast spline evaluation procedure; tidy up output

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(function () {
   window.r = Raphael('holder');
-  window.rowTemplate = _.template("<tr><td><input type='input' class='xin' value='<%= x %>'/></td><td><input type='input' class='yin' value='<%= y %>'/></td><td><a href='#' class='remove'>X</a></td></tr>");
+  window.rowTemplate = _.template($('script#rowTemplate').html());
+  window.dataTemplate = _.template($('script#dataTemplate').html());
 
   $('#values').on('change', 'input', function() {
     redraw();
@@ -68,7 +69,8 @@ window.drawGraph = function(r, x, y) {
   }
 
   var coefs = spline_coef(x, y);
-  var points = spline_plot(x, y, coefs);
+  var expanded_coefs = expand_coefs(x, y, coefs);
+  var points = spline_plot(x, y, expanded_coefs);
   window.chart = r.linechart(20, 0, 900, 550, [x, points[0]], [y, points[1]],
                               {
                                 axis: '0 0 1 1', axisxstep: niceSteps(x), axisystep: niceSteps(y),
@@ -81,7 +83,7 @@ window.drawGraph = function(r, x, y) {
                                 addRow(pt[0], pt[1]);
                                 redraw();
                               });
-  $('#results').val(coefs.toString());
+  $('#results').val(dataTemplate({data: expanded_coefs}));
 };
 
 window.getX = function() {
@@ -157,7 +159,7 @@ window.spline_coef = function(x, y) {
   return z;
 }
 
-window.spline_interp = function(x_eval, x, y, coefs) {
+window.spline_interp_direct = function(x_eval, x, y, coefs) {
   // evaluate the natural cubic spline given by x, y and coefs at x_eval
 
   var n = x.length - 1;
@@ -193,6 +195,50 @@ window.spline_interp = function(x_eval, x, y, coefs) {
   return y[i]+diff*tmp;
 }
 
+window.spline_interp = function(x_eval, coefs) {
+  // evaluate the spline represented by the coefs array
+  // see expand_coefs for details on the format
+  var n = coefs.length - 1;
+  var diff;
+  var i;
+
+  for(i=n; i > 0; i--) {
+    diff = x_eval - coefs[i][0];
+    if (diff >= 0) {
+      break;
+    }
+  }
+  // note: if we complete the loop without breaking, i will be 0
+  var c = coefs[i][1];
+  return c[0] + diff*(c[1] + diff*(c[2] + diff*c[3]));
+}
+
+window.expand_coefs = function(x, y, coefs) {
+  // find the coefficients of a chained multiplication explictly
+  // each row of the result looks like:
+  //   [x0, [a, b, c, d]]
+  // which is a cubic segment starting at x0 of the form:
+  //   y = a + diff*(b+diff*(c+d*diff))
+  // where diff = x - x0
+  //
+  // the first and last elements in the array encode the coefficients
+  // needed to linearly extrapolate the spline outside the original bounds.
+  var n = x.length - 1;
+  var h = x[1] - x[0];
+  var dy = y[1] - y[0];
+  var result = [[x[0], [y[0], (dy/h - h*(coefs[1] + 2*coefs[0])/6), 0, 0]]];
+  for(var i=0; i<n; i++) {
+    h = x[i+1] - x[i];
+    dy = y[i+1] - y[i];
+    var dz = coefs[i+1] - coefs[i];
+    result.push([x[i], [y[i], (dy/h - h*(coefs[i+1]+2*coefs[i])/6), coefs[i]/2, dz/(6*h)]]);
+  }
+  dy = y[n] - y[n-1];
+  h = x[n] - x[n-1];
+  result.push([x[n], [y[n], (dy/h + h*(2*coefs[n] + coefs[n-1])/6), 0, 0]]);
+  return result;
+}
+
 window.spline_plot = function(x, y, coefs) {
   // return an array with an array of x coordinates and an array of y coordinates for the
   // graph of the spline specified by x, y and coefs
@@ -208,7 +254,7 @@ window.spline_plot = function(x, y, coefs) {
   for(var i=0; i<n; i++) {
     var x0 = i*inc + xmin;
     points[0][i] = x0;
-    points[1][i] = spline_interp(x0, x, y, coefs);
+    points[1][i] = spline_interp(x0, coefs);
   }
 
   return points;
